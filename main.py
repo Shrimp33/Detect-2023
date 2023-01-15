8d03291929c933a17b7abe4d7bb8ff44cd48adf1
Shrimp33 <Junru.Chen.2007@gmail.com>
2023-01-15 10:10:33 -0500

    debug

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 channel = MCP3008(channel=0, clock_pin=18, mosi_pin=24, miso_pin=23, select_pin=25)
 
 while True:
-    logging.debug(channel.value)
+    logging.debug(f"Reading from channel {channel.value}")
+    logging.debug(f"Raw Voltage: {channel.voltage}")
     res = TODO(channel.value)
     sleep(DELAY)
