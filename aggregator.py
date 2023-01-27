3ad4743ce8953bd3b69c3c202de6796e453745ad
Jun Ru Chen <junru.chen.2007@gmail.com>
2023-01-27 15:08:07 -0500

    calibration

--- aggregator.py
+++ aggregator.py
@@ -1,2 +1,30 @@
-def TODO(n):
-    return None
+import time
+from gpiozero import LED
+
+# Line of best fit 1.72 * 10^-3 x + 0.0553
+# R^2 = 0.934
+
+def run(n):
+    red = LED(16)
+    green = LED(20)
+    blue = LED(21)
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+
+    mass = 1.72 * pow(10, -3) * n + 0.0553
+    print(mass + "g")
+
+    if mass > RED:
+        red.off()
+        green.on()
+        blue.off
+    elif mass > GREEN:
+        red.off()
+        green.off()
+        blue.on()
+    else:
+        red.on()
+        green.on
+        blue.on()
+    
--- main.py
+++ main.py
@@ -5,13 +5,28 @@ from aggregator import *
 import logging
 
 DELAY = 0.5
+TOLERANCE = 0.02
 
 logging.basicConfig(level=logging.DEBUG)
 
 channel = MCP3008(channel=0, clock_pin=18, mosi_pin=24, miso_pin=23, select_pin=25)
 
+prev = 0
+res = 0
+rest = time.time()
+adj = 0
+
 while True:
-    logging.debug(f"Reading from channel {channel.value}")
-    logging.debug(f"Raw Voltage: {channel.voltage}")
-    res = TODO(channel.value)
-    sleep(DELAY)
+    # logging.debug(f"Reading from channel {channel.value}")
+    # logging.debug(f"Raw Voltage: {channel.voltage}")
+    raw = TODO(channel.value)
+    t = time.time()
+    draw = raw - prev
+    dt = t - rest
+    if draw != 0:
+        rest = t
+    slope = draw / dt
+    if slope > TOLERANCE:
+        adj = raw
+    run(adj)
+    time.sleep(DELAY)
